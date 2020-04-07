0715418252e39c8566df19757f55eb272341cbb2
phoenixSP <pal00007@umn.edu>
2020-04-06 23:22:32 -0500

    Update factory.py

--- Ch02/02_02/factory.py
+++ Ch02/02_02/factory.py
@@ -1,9 +1,36 @@
 class Dog:
+    """A simple dog class"""
 
-	"""A simple dog class"""
+    def __init__(self, name):
+        self._name = name
+    
+    def speak(self):
+        return "Woof!"
+    
+class Cat:
+    """A simple cat class"""
 
-	def __init__(self, name):
-		self._name = name
+    def __init__(self, name):
+        self._name = name
+    
+    def speak(self):
+        return "Meow!"
+    
+def get_pet(pet = "dog"):
+    """ factory method """
+    
+    pets = {'dog' : Dog("Hope"), 'cat': Cat("Peace")}
+#    pets = dict(dog = Dog("Hope"))
+#    print(pets)
+    return pets[pet]
+    
+d = get_pet("dog")
+print(d.speak())
 
-	def speak(self):
-		return "Woof!"
+c = get_pet("cat")
+print(c.speak())
+    
+    
+
+    
+    
