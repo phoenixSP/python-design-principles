dbb176f7a9827f89d30e7032ab97572586be0e66
phoenixSP <pal00007@umn.edu>
2020-04-06 23:53:19 -0500

    updating abstract Factory and singleton

--- Ch02/02_04/abstractFactory.py
+++ Ch02/02_04/abstractFactory.py
@@ -9,39 +9,40 @@ class Dog:
 
 
 class DogFactory:
-	"""Concrete Factory"""
-
-	def get_pet(self):
-		
-
-	
-	def get_food(self):
+    """Concrete Factory"""
+    
+    def get_pet(self):
+        """"Returns a DOg Object"""
+        return Dog()
+    
+    def get_food(self):
+        return "Dog Food!"
 		
 
 
 
 class PetStore:
-	""" PetStore houses our Abstract Factory """
+    """ PetStore houses our Abstract Factory """
+    
+    def __init__(self, pet_factory=None):
+        """ pet_factory is our Abstract Factory """
+        self.__pet_factory = pet_factory
+        
+    def show_pet(self):
+        """ Utility method to display the details of the objects retured by the DogFactory """
+        pet = self.__pet_factory.get_pet()
+        pet_food = self.__pet_factory.get_food()
 
-	def __init__(self, pet_factory=None):
-		""" pet_factory is our Abstract Factory """
-
-
-
-	def show_pet(self):
-		""" Utility method to display the details of the objects retured by the DogFactory """
-
-
-		
-		print("Our pet is '{}'!".format(pet))
-		print("Our pet says hello by '{}'".format(pet.speak()))
-		print("Its food is '{}'!".format(pet_food))
+        print("Our pet is '{}'!".format(pet))
+        print("Our pet says hello by '{}'".format(pet.speak()))
+        print("Its food is '{}'!".format(pet_food))
 
 
 #Create a Concrete Factory
-
+factory = DogFactory()
 
 #Create a pet store housing our Abstract Factory
-
+shop = PetStore(factory)
 
 #Invoke the utility method to show the details of our pet
+shop.show_pet()
--- Ch02/02_06/singleton.py
+++ Ch02/02_06/singleton.py
@@ -1,12 +1,25 @@
 class Borg:
-    
     """The Borg design pattern"""
     
+    _shared_state = {}
+    
+    def __init__(self):
+        self.__dict__ = self._shared_state 
 
-        
 
 class Singleton(Borg): 
-    
     """The singleton class"""
-   
+    
+    def __init__(self, **kwargs):
+        Borg.__init__(self)
+        self._shared_state.update(kwargs)
+        
+    def __str__(self):
+        return str(self._shared_state)
+    
+    
+x = Singleton(HTTP = "Hyper Text and Transfer Protocol")
+print(x)
 
+y = Singleton(SNMP = "Something")
+print(y)
